ad2f28f4a0809e2e5b7d324e614316908274ea87
[Alireza-Hassani-Eghtedar] <[alirezaprogrammer2001@gmail.com]>
2023-08-16 00:59:04 +0430

    update project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// SIDE NAVIGATION JS CODE 
+
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn = document.querySelector(".cancel-btn");
 let navBar = document.querySelector(".navbar");
@@ -11,4 +13,16 @@ cancelBtn.onclick = function() {
     menuBtn.style.opacity = "1";
     menuBtn.style.pointerEvents = "auto";
     navBar.classList.remove("active");
+}
+
+// STICKY NAVIGATION JS CODE 
+
+let nav = document.querySelector("nav");
+let val;
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("sticky");
+    } else {
+        nav.classList.remove("sticky");
+    }
 }
